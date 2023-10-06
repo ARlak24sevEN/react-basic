e67ecd0c4ae69d555d6d2816287c4ca1c8df6c2d
arlak <arlak_a@muangthai.co.th>
2023-10-06 15:09:53 +0700

    use state arrow function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
-  function addCount() {
-    console.log(count);
-    setCount(count + 1);
-  }
+  // function addCount() {
+  //   console.log(count);
+  //   setCount(count + 1);
+  // }
 
-  function reduceCount() {
-    console.log(count);
-    setCount(count - 1);
-  }
-  function resetCount() {
-    console.log(count);
-    setCount(0);
-  }
+  // function reduceCount() {
+  //   console.log(count);
+  //   setCount(count - 1);
+  // }
+  // function resetCount() {
+  //   console.log(count);
+  //   setCount(0);
+  // }
 
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={addCount}>add count</button>
-      <button onClick={reduceCount}>reduce count</button>
-      <button onClick={resetCount}>reset</button>
+      <button onClick={()=>setCount(count+1)}>add count</button>
+      <button onClick={()=>setCount(count-1)}>reduce count</button>
+      <button onClick={()=>setCount(0)}>reset</button>
     </>
   );
 }
