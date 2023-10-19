759d05ee22cd942a04a14f5e933ce30f9a845954
arlak <arlak_a@muangthai.co.th>
2023-10-19 15:41:17 +0700

    components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function  Header () {
+    return (
+        <h1>Arlak</h1>
+    )
+}
+export default Header
+
+
+//export default function Header(){}
+
+// const Header=()=>{} export default Header
---
+++ src/components/StudentList.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+export default function StrudentList(){
+    const [students, setStudent] = useState([
+        { id: 1, name: "arlak" },
+        { id: 2, name: "nadia" },
+        { id: 3, name: "anna" },
+      ]);
+    
+      const [show,setShow] = useState(true)// true show , false no show
+    
+      function deleteStudent(id){
+       setStudent(students.filter(item => item.id !== id ))
+      }
+      return (
+        <>
+        <h1> จำนวนนักเรียน = {students.length}</h1>
+      
+        <button onClick={()=>setShow(!show)}> สลับ</button>
+        <ul>
+          {show && students.map((item)=>(
+            <li key={item.id}>
+              <p>{item.id} - {item.name}</p>
+              <button onClick={()=>deleteStudent(item.id)}>ลบ</button>
+            </li>
+          ))}
+        </ul>
+        </>
+      );
+}
